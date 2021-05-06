9bfd255c3d548c48a407e6c0dbd72893770c5ffc
funkyfourier <johs@ringheimsauto.org>
2021-05-06 15:02:20 +0200

    Added outlets for angle values

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mpu6050rpi
-Pure Data external for the MPU6050 motion sensor
+Pure Data external for the MPU6050 motion sensor.
+
+Due to my limited knowledge of C, the code could probably use some restructuring. Please do refactor! School me!
--- mpu6050rpi.c
+++ mpu6050rpi.c
@@ -6,17 +6,28 @@ static t_class *mpu6050rpi_class;
 typedef struct _mpu6050rpi
 {
     t_object x_obj;
+    t_outlet *angle_x_out;
+    t_outlet *angle_y_out;
+    t_outlet *angle_z_out;
 } t_mpu6050rpi;
 
 void mpu6050rpi_bang(t_mpu6050rpi *x)
 {
     post("Hello world !!");
+    outlet_float(x->angle_x_out, 212121);
+    outlet_float(x->angle_y_out, -212121);
+    outlet_float(x->angle_z_out, 0);
 }
 
 void *mpu6050rpi_new(void)
 {
     init_mpu6050();
     t_mpu6050rpi *x = (t_mpu6050rpi *)pd_new(mpu6050rpi_class);
+
+    x->angle_x_out = outlet_new(&x->x_obj, &s_float);
+    x->angle_y_out = outlet_new(&x->x_obj, &s_float);
+    x->angle_z_out = outlet_new(&x->x_obj, &s_float);
+
     return x;
 }
 
